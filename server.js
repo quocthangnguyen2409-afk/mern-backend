3f4ae4a0d61a5e1f759151db65f3f03820799fc5
Nguyen Van A <nguyenvana@gmail.com>
2026-03-20 16:51:40 +0700

    Fix env and gitignore files

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('MERN Backend is running!');
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
